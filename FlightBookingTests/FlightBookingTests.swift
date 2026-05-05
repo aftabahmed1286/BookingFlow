27a1fbb2d1dd44a15e301185ba8bab07aa0acb2c
Aftab Ahmed <aftab1286@gmail.com>
2026-05-05 10:23:40 +0530

    Initial Commit

---
+++ FlightBookingTests/FlightBookingTests.swift
@@ -0,0 +1,18 @@
+//
+//  FlightBookingTests.swift
+//  FlightBookingTests
+//
+//  Created by Aftab on 05/05/26.
+//
+
+import Testing
+
+struct FlightBookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
